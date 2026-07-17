d09042ed0112623b2b7c8285c622b361ed92fe21
rifqi pangestu <rifqipangestu11@gmail.com>
2026-07-17 13:01:58 +0700

    Improve Vercel production readiness

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\LoanController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return redirect()->route('login');
+    return view('welcome');
 });
 
 Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
