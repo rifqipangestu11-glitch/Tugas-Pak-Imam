b1520627d927d1644c71dfc7ee51523c93a9ffe1
rifqi pangestu <rifqipangestu11@gmail.com>
2026-07-17 12:53:50 +0700

    Add Vercel deployment config

---
+++ api/index.php
@@ -0,0 +1,16 @@
+<?php
+
+$_ENV['APP_BASE_PATH'] = dirname(__DIR__);
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$request = Illuminate\Http\Request::capture();
+$response = $kernel->handle($request);
+
+$response->send();
+
+$kernel->terminate($request, $response);
